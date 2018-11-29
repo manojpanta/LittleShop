4c7eae76aa1f42caeeafa6d4cec553e2ed1c1192
manojpanta <manojpanta95@gmail.com>
2018-11-29 08:12:18 -0700

    added show page for user which show just name of the user right now

--- spec/features/visitor_can_create_new_user_spec.rb
+++ spec/features/visitor_can_create_new_user_spec.rb
@@ -10,5 +10,12 @@ describe 'visitor visiting home page' do
     click_on 'Register'
 
     expect(current_path).to eq(new_user_path)
+
+    fill_in :user_username, with: username
+    fill_in :user_password, with: password
+
+    click_on 'Create User'
+
+    expect(page).to have_content("Welcome #{username}!!")
   end
 end
